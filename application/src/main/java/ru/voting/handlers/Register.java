bba686344ef00dde291304872de1504d47856b30
Valeria Bulanova <vvbulanova@edu.hse.ru>
2023-04-18 22:18:04 +0300

    Added LogIn and Register handlers

---
+++ application/src/main/java/ru/voting/handlers/Register.java
@@ -0,0 +1,31 @@
+package ru.voting.handlers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import ru.voting.common.Poll;
+import ru.voting.common.User;
+
+import java.util.Map;
+
+@Controller
+public class Register {
+    @Autowired
+    private Map<String, User> users;
+
+    @PostMapping(
+            value = "/register",
+            consumes = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<String> register(@RequestBody User newUser) {
+        if (users.containsKey(newUser.getEmail())) {
+            return new ResponseEntity<>("User with such an email already exists", HttpStatus.BAD_REQUEST);
+        }
+        users.put(newUser.getEmail(), newUser);
+        return new ResponseEntity<>("Email " + newUser.getEmail() + " successfully registered!", HttpStatus.OK);
+    }
+}
--- application/src/test/java/ru/voting/handlers/GetPollTest.java
+++ application/src/test/java/ru/voting/handlers/GetPollTest.java
@@ -58,3 +58,7 @@ public class GetPollTest {
                 .andExpect(content().string(poll.toString()));
     }
 }
+
+
+
+
