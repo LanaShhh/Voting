2abdcf1cf3a92151033ba0ceec5d06e77145c9a9
Damir Petrov <dppetrov@edu.hse.ru>
2023-04-19 20:34:39 +0300

    rename GetPoll

--- application/src/main/java/ru/voting/handlers/GetPollController.java
+++ application/src/main/java/ru/voting/handlers/GetPollController.java
@@ -1,18 +1,18 @@
 package ru.voting.handlers;
 
+import org.springframework.web.bind.annotation.RestController;
 import ru.voting.common.Poll;
 import ru.voting.emails.EmailService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Map;
 
-@Controller
-public class GetPoll {
+@RestController
+public class GetPollController {
     @Autowired
     private EmailService emailService;
     @Autowired
