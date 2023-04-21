1ded8d856a9211022047949dd004ce1658a0287a
LanaShhh <lana.shhh@yandex.ru>
2023-04-21 13:17:56 +0300

    Controller to RestController

--- application/src/main/java/ru/voting/controllers/GetInfoController.java
+++ application/src/main/java/ru/voting/controllers/GetInfoController.java
@@ -4,16 +4,17 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import ru.voting.common.User;
 import ru.voting.storage.DatabaseService;
 
-@Controller
+@RestController
 public class GetInfoController {
     @Autowired
     private DatabaseService databaseService;
+
     @GetMapping("/get_info")
     public ResponseEntity<String> getInfo(@RequestParam String email) {
         if (email == null || email.equals("")) {
